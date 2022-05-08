057e1260383a65c99f6a744f1425adb757a80aee
zbs-ha <2260402857@qq.com>
2022-05-08 19:11:01 +0800

    组件的依赖项

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-var-requires': 'off'
+    '@typescript-eslint/no-var-requires': 'off',
+    'dot-notation': 'off'
   },
 }
